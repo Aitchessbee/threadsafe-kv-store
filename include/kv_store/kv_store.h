0a22d85603b180dc6757d7bc0fd6924ca6fa7a5e
Aitchessbee <bediharsiddak@gmail.com>
2026-01-02 14:32:30 +0530

    Add sharding with one mutex per shard

--- include/kv_store/kv_store.h
+++ include/kv_store/kv_store.h
@@ -3,19 +3,29 @@
 #include <string>
 #include <unordered_map>
 #include <shared_mutex>
+#include <vector>
 #include <utility>
 
 namespace kv_store {
-    
+
 class KVStore {
 public:
+    explicit KVStore(size_t num_shards = 16);
+
     std::pair<bool, std::string> get(const std::string& key) const;
     void put(const std::string& key, const std::string& value);
     void erase(const std::string& key);
 
 private:
-    std::unordered_map<std::string, std::string> store_;
-    mutable std::shared_mutex mutex_;
+    struct Shard {
+        mutable std::shared_mutex mutex;
+        std::unordered_map<std::string, std::string> map;
+    };
+
+    size_t num_shards_;
+    std::vector<Shard> shards_;
+
+    size_t getShardIndex(const std::string& key) const;
 };
 
 }
--- src/kv_store.cpp
+++ src/kv_store.cpp
@@ -1,26 +1,40 @@
-#include <string>
 #include "../include/kv_store/kv_store.h"
+#include <functional>
 
 namespace kv_store {
-    void KVStore::put(const std::string &key, const std::string &value) {
-        std::unique_lock<std::shared_mutex> lock(mutex_);
-        store_[key] = value;
-    }
 
-    std::pair<bool, std::string> KVStore::get(const std::string &key) const {
-        std::shared_lock<std::shared_mutex> lock(mutex_);
-        auto it = store_.find(key);
+KVStore::KVStore(size_t num_shards)
+    : num_shards_(num_shards), shards_(num_shards) {}
 
-        if(it == store_.end()) {
-            return {false, ""};
-        }
+size_t KVStore::getShardIndex(const std::string& key) const {
+    std::hash<std::string> hasher;
+    return hasher(key) % num_shards_;
+}
 
-        return {true, it->second};
-    }
+void KVStore::put(const std::string& key, const std::string& value) {
+    size_t shard_index = getShardIndex(key);
+    Shard& shard = shards_[shard_index];
 
-    void KVStore::erase(const std::string &key) {
-        std::unique_lock<std::shared_mutex> lock(mutex_);
-        
-        store_.erase(key);
-    }
+    std::unique_lock<std::shared_mutex> lock(shard.mutex);
+    shard.map[key] = value;
+}
+
+std::pair<bool, std::string> KVStore::get(const std::string& key) const {
+    size_t shard_index = getShardIndex(key);
+    const Shard& shard = shards_[shard_index];
+
+    std::shared_lock<std::shared_mutex> lock(shard.mutex);
+    auto it = shard.map.find(key);
+    if (it == shard.map.end()) return {false, ""};
+    return {true, it->second};
+}
+
+void KVStore::erase(const std::string& key) {
+    size_t shard_index = getShardIndex(key);
+    Shard& shard = shards_[shard_index];
+
+    std::unique_lock<std::shared_mutex> lock(shard.mutex);
+    shard.map.erase(key);
+}
+
 }
